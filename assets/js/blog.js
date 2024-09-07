a87a47ea7be45cf41fdfeeba36c94deb2adc5ada
topal99 <muhamamdtaufal@students.unnes.ac.id>
2024-09-07 21:50:39 +0700

    Day 6 - Responsive Layout

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const menuToggleButton = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuToggleButton.addEventListener('click', function() {
+    if (mobileMenu.classList.contains('mobile-menu-active')) {
+      mobileMenu.classList.remove('mobile-menu-active');
+    } else {
+      mobileMenu.classList.add('mobile-menu-active');
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('projectForm');
     const projectCardsContainer = document.querySelector('.project-cards');
     let editIndex = null; // Track project being edited
