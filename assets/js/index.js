041d0e57a2fe07ae80e55acf274e038fd6cfe279
topal99 <muhamamdtaufal@students.unnes.ac.id>
2024-09-07 14:39:25 +0700

    Day 1 - Creating Card

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggleButton = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+  
+    menuToggleButton.addEventListener('click', function() {
+      if (mobileMenu.classList.contains('mobile-menu-active')) {
+        mobileMenu.classList.remove('mobile-menu-active');
+      } else {
+        mobileMenu.classList.add('mobile-menu-active');
+      }
+    });
+  });
